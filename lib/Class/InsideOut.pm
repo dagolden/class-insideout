691381036b8dad94b9761ddb085b390e6478b6d8
David Golden <dagolden@cpan.org>
2006-09-09 16:55:09 +0000

    r3884@vesta (orig r410):  david | 2006-06-09 02:46:58 -0400  import of distributions skeleton
    git-svn-id: https://dagolden.googlecode.com/svn/Class-InsideOut/trunk@422 dfce27d5-b31c-0410-bb09-030b4413eeba

---
+++ lib/Class/InsideOut.pm
@@ -0,0 +1,68 @@
+package Class::InsideOut;
+use strict;
+use warnings;
+use Carp;
+
+BEGIN {
+    use Exporter ();
+    use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+    $VERSION     = "0.10";
+    @ISA         = qw (Exporter);
+    @EXPORT      = qw ();
+    @EXPORT_OK   = qw ();
+    %EXPORT_TAGS = ();
+}
+
+#--------------------------------------------------------------------------#
+# main pod documentation 
+#--------------------------------------------------------------------------#
+
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+Class::InsideOut - Put abstract here 
+
+=head1 SYNOPSIS
+
+ use Class::InsideOut;
+ blah blah blah
+
+=head1 DESCRIPTION
+
+Description...
+
+=head1 USAGE
+
+Usage...
+
+=cut
+
+
+1; #this line is important and will help the module return a true value
+__END__
+
+=head1 BUGS
+
+Please report bugs using the CPAN Request Tracker at L<http://rt.cpan.org/>
+
+=head1 AUTHOR
+
+David A. Golden (DAGOLDEN)
+
+dagolden@cpan.org
+
+http://dagolden.com/
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006 by David A. Golden
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=cut
---
+++ t/01_Class_InsideOut.t
@@ -0,0 +1,6 @@
+# Class::InsideOut - check module loading and create testing directory
+
+use Test::More tests =>  1 ;
+
+BEGIN { use_ok( 'Class::InsideOut' ); }
+
